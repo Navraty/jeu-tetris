ebed7b316ef40333efd5f8467ee7a13ac505c383
Navraty <tsamezojanne@gmail.com>
2024-01-25 16:02:01 +0100

    tetris

---
+++ html_server.js
@@ -0,0 +1,37 @@
+function start_html_server() {
+  const http = require('http');
+  const fs = require('fs');
+
+  const hostname = '0.0.0.0';
+  const port = 8080;
+
+  const server = http.createServer(function(request, response) {
+    let extension = '.' + request.url;
+    let re = extension.match(/([.])\w+/g);
+    let type = (re === null) ? '.html' : re[0];
+    let path = (extension === './') ? './index.html' : extension;
+    let contentType = '';
+    switch (type) {
+      case '.html':
+        contentType = 'text/html';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+    }
+    console.log(path);
+    response.writeHead(200, {"Content-Type": contentType});
+    response.write(fs.readFileSync(path, 'utf8'));
+    response.end();
+  }).listen(port, hostname, () => {
+      console.log("Server (Local): port 8080");
+      console.log("Server (DoCode) running at http://akafack.docode.qwasar.io");
+      // console.log("Replace XXXXXXXXX by your current workspace ID");
+      // console.log("(look at the URL of this page and akafack_s.docode.qwasar.io, XXXXXXXXX is your workspace ID)");
+  });
+}
+
+start_html_server();
---
+++ script.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+
+// Utilisez express dans votre code...
